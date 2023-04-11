d5639c3d0075effddac2b159d273793b208f52db
Will Robinson <will@oznetnerd.com>
2023-04-11 12:46:13 +1000

    Added account name options

--- README.md
+++ README.md
@@ -6,24 +6,35 @@ The script iterates over customers' accounts and identifies the ones which have 
 
 # Usage Instructions
 
-1. Navigate to "Compute" --> "System"
-2. Click "Utilities". Take note of the "Path to Console" and URL and the "API token".
+1. Navigate to **"Compute"** --> **"System"**
+2. Click "Utilities". Take note of the "Path to Console" and URL and the **"API token"**.
 3. Set the `CWP_TOKEN` environment variable to the "API token" found in the previous step.
 4. Run the script using the following syntax:
 
 ```
-python3 agentless_tagger.py <PATH_TO_CONSOLE_URL> <NUMBER_OF_ACCOUNTS> <KEY_VALUE_PAIRS>
+python3 agentless_tagger.py <PATH_TO_CONSOLE_URL> <NUMBER_OF_ACCOUNTS/ACCOUNT_NUMBERS> <KEY_VALUE_PAIRS>
 ```
 
-Example:
+## Example #1
+Update **1** Prisma Cloud account by replacing `<NUMBER_OF_ACCOUNTS/ACCOUNT_NUMBERS>` with `1`:
 
 ```
-python3 agentless_tagger.py https://us-east1.cloud.twistlock.com/us-1-111111111 0 name1:key1,name2:key2,name3:key3
+python3 agentless_tagger.py https://us-east1.cloud.twistlock.com/us-1-111111111 1 name1:key1,name2:key2,name3:key3
 ```
 
-**Note:** `<NUMBER_OF_ACCOUNTS>` is used to control how many accounts the script updates. It can be used for testing purposes and implementing the change in batches. Setting it to `0` will ensure all accounts get updated.
+**Note:** `<NUMBER_OF_ACCOUNTS/ACCOUNT_NUMBERS>` is used to control how many accounts the script updates. It can be used for testing purposes and implementing the change in batches. Setting it to `0` will ensure all accounts get updated.
 
+## Example #2
+Update specific accounts by replacing `<NUMBER_OF_ACCOUNTS/ACCOUNT_NUMBERS>` with a comma separated list of account numbers:
+
+```
+python3 agentless_tagger.py https://us-east1.cloud.twistlock.com/us-1-111111111 222222222222,333333333333 name1:key1,name2:key2,name3:key3
+```
 
 # Known issues
 
 Pagination has not been implemented yet. Therefore, only a maximum of 50 accounts will be updated at this point in time.
+
+# Disclaimer
+
+This code is not provided, nor maintained by Palo Alto Networks. It is Proof of Concept (POC) code and must not be considered production ready.
--- src/agentless_tagger.py
+++ src/agentless_tagger.py
@@ -10,16 +10,25 @@ requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 SCAN_API_URL = "api/v1/cloud-scan-rules?project=Central+Console"
 
 
-def _get_update_account_count():
+def _get_update_account_info():
     try:
-        update_account_count = int(sys.argv[2])
+        get_user_input = int(sys.argv[2])
 
     except IndexError:
         sys.exit(
-            "Error: Please enter the number of accounts you want to modify. Use 0 update all accounts"
+            "Error: Please enter a comma separated list of account numbers, or the number of accounts you want to modify. Use 0 if you'd like to update all accounts"
         )
 
-    return update_account_count
+    if isinstance(get_user_input, int) and len(str(get_user_input)) != 12:
+        update_account_numbers = None
+        update_account_count = get_user_input
+
+    else:
+        update_account_numbers = str(get_user_input).split(",")
+        update_account_count = len(update_account_numbers)
+
+
+    return update_account_numbers, update_account_count
 
 
 def _get_base_url():
@@ -91,13 +100,16 @@ def add_agentless_custom_tags(headers, base_url, new_cwp_config_payload):
     return response
 
 
-def _build_new_cwp_config_payload(
-    update_account_count, new_cwp_configs, new_custom_tags
+def _build_new_cwp_config_payload(new_cwp_configs, new_custom_tags, update_account_numbers, update_account_count
 ):
     new_cwp_config_payload = []
 
     for new_cwp_config in new_cwp_configs:
         if not new_cwp_config["agentlessScanSpec"]["enabled"]:
+            continue
+
+        if update_account_numbers and new_cwp_config["credential"]["accountID"] not in update_account_numbers:
+            print(new_cwp_config["credential"]["accountID"])
             continue
 
         if new_cwp_config["agentlessScanSpec"].get("customTags"):
@@ -130,7 +142,7 @@ def _print_report(new_cwp_config_payload):
 
 
 def main():
-    update_account_count = _get_update_account_count()
+    update_account_numbers, update_account_count = _get_update_account_info()
     base_url = _get_base_url()
     customer_url = f"{base_url}/{SCAN_API_URL}"
     token = _get_token()
@@ -144,7 +156,7 @@ def main():
     original_cwp_configs = get_cwp_configs(headers, customer_url)
     new_cwp_configs = deepcopy(original_cwp_configs)
     new_cwp_config_payload = _build_new_cwp_config_payload(
-        update_account_count, new_cwp_configs, new_custom_tags
+        new_cwp_configs, new_custom_tags, update_account_numbers, update_account_count
     )
 
     add_agentless_custom_tags(headers, customer_url, new_cwp_config_payload)
